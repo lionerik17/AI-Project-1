4ee0467aeeb9a3f2d09306cd523ab32acfd4dd77
lionerik17 <pufferstar17@gmail.com>
2024-11-30 18:11:27 +0200

    added pacman search agents

--- doc_ia/code/suboptimal_search.py
+++ doc_ia/code/suboptimal_search.py
@@ -1,3 +1,8 @@
+def isGoalState(self, state: Tuple[int, int]):
+    x,y = state
+
+    return self.food[x][y]
+
 def findPathToClosestDot(gameState):
     startPosition = gameState.getPacmanPosition()
     food = gameState.getFood()
